b623148f036a0cc7eafc132e82fa9d9ecc423071
Mauricio Paiva Reis <85911362+mauricioPReis@users.noreply.github.com>
2023-03-03 17:28:11 -0300

    Atualização.2

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function calcularFatura() {
   // chamando os valor de html
   let valorPlano = document.getElementById('valorPlano').value;
-  let diasDeUso = document.getElementById('diasDeUso').value;
+  let initialDate = new Date (document.getElementById('initialDate').value);
+  let finalDate = new Date(document.getElementById('finalDate').value);
+
+  //convertendo para milesegundos
+  const dataDifEmMilessegundos = Math.abs(initialDate.getTime() - finalDate.getTime());
+  
+  // Converter a diferença em dias
+  const diferencaEmDias = Math.ceil(dataDifEmMilessegundos / (1000 * 60 * 60 * 24)); 
 
   // Verifica se os valores passados como argumento são números
   if (isNaN(valorPlano) || isNaN(diasDeUso)) {
@@ -9,16 +16,24 @@ function calcularFatura() {
   }
 
   //realizando os calculos de dias
-  const valorDiario = valorPlano / 30;
-  const valorProporcional = valorDiario * diasDeUso;
-
+  let faturaPropDesc = valorPlano - valorProporcional
+	let faturaProp = parseFloat(valorPlano) + parseFloat(valorProporcional)
+  
   let proportionalValue = document.getElementById('resul').textContent = valorProporcional.toFixed(2);
-
-  return proportionalValue
+  
+  let valueDesconto = document.getElementById('resul2').textContent = faturaPropDesc.toFixed(2);
+  
+  let faturaUnificadaValue = document.getElementById('resul3').textContent = faturaProp.toFixed(2);
+  
+  return proportionalValue, valueDesconto, faturaUnificadaValue
 }
 
 function clearInputs(){
   document.getElementById('valorPlano').value = "";
-  document.getElementById('diasDeUso').value = "";
-  document.getElementById('resul').value = "";
+  document.getElementById('initialDate').value = "";
+  document.getElementById('finalDate').value = "";
+    
+  document.getElementById('resul').innerHTML="0.00";
+  document.getElementById('resul2').innerHTML="0.00";
+  document.getElementById('resul3').innerHTML="0.00";
 }
